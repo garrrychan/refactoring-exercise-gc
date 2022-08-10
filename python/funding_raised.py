4229b17eb8b68016ba8f365f2955fbc5f2ac4420
Garry <chan6974@gmail.com>
2022-08-10 11:04:39 -0400

    refactored further

--- python/funding_raised.py
+++ python/funding_raised.py
@@ -1,126 +1,29 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 import pandas as pd
-
 
 class FundingEvent:
 
     def __init__(self, file_path):
         self.file_path = file_path
 
-    def filter_funding_events(self, fields: Dict) -> List[Dict]:
+    def filter_funding_events(self, filter_dict: Dict, is_first_event: bool) -> Union[List[Dict], Dict]:
         """
-        :param fields: Dict where key represents the csv field column and value represents the str
-        to filter against
-        :return: List of Dict where each Dict represents a funding event row.
+        :param filter_dict: Dict where key represents the csv field column and value represents the str
+        to filter against. Options are: company_name, city, state, round
+        :return: Return funding row events that match the filter dict.
         """
         df = pd.read_csv(self.file_path)
-        if 'company_name' in fields:
-            df = self.funding_events_by_company(df, fields.get('company_name'))
-        if 'city' in fields:
-            df = self.funding_events_by_city(df, fields.get('city'))
-        if 'state' in fields:
-            df = self.funding_events_by_state(df, fields.get('state'))
-        if 'round' in fields:
-            df = self.funding_events_by_round(df, fields.get('round'))
-        return df.to_dict('records')
+        for key, value in filter_dict.items():
+            df = df[df[key] == value]
+        return self.format_rows(df, is_first_event)
 
-    @staticmethod
-    def funding_events_by_company(df: pd.DataFrame, company_name: str) -> pd.DataFrame:
-        df = df[df['company_name'] == company_name]
-        return df
+    def format_rows(self, df: pd.DataFrame, is_first_event: bool) -> Union[List[Dict], Dict]:
+        if not is_first_event:
+            return df.to_dict('records')
+        elif not df.empty:
+            return df.head(1).to_dict('records')[0]
+        else:
+            raise RecordNotFound("This funding event does not exist.")
 
-    @staticmethod
-    def funding_events_by_city(df: pd.DataFrame, city: str) -> pd.DataFrame:
-        df = df[df['city'] == city]
-        return df
-
-    @staticmethod
-    def funding_events_by_state(df: pd.DataFrame, state: str) -> pd.DataFrame:
-        df = df[df['state'] == state]
-        return df
-
-    @staticmethod
-    def funding_events_by_round(df: pd.DataFrame, state: str) -> pd.DataFrame:
-        df = df[df['round'] == state]
-        return df
-
-
-#     @staticmethod
-#     def find_by(options):
-#         with open("../startup_funding.csv", "rt") as csvfile:
-#             data = csv.reader(csvfile, delimiter=',', quotechar='"')
-#             # skip header
-#             next(data)
-#             csv_data = []
-#             for row in data:
-#                 csv_data.append(row)
-#
-#         if 'company_name' in options:
-#             for row in csv_data:
-#                 if row[1] == options['company_name']:
-#                     mapped = {}
-#                     mapped['permalink'] = row[0]
-#                     mapped['company_name'] = row[1]
-#                     mapped['number_employees'] = row[2]
-#                     mapped['category'] = row[3]
-#                     mapped['city'] = row[4]
-#                     mapped['state'] = row[5]
-#                     mapped['funded_date'] = row[6]
-#                     mapped['raised_amount'] = row[7]
-#                     mapped['raised_currency'] = row[8]
-#                     mapped['round'] = row[9]
-#                     return mapped
-#
-#         if 'city' in options:
-#             for row in csv_data:
-#                 if row[4] == options['city']:
-#                     mapped = {}
-#                     mapped['permalink'] = row[0]
-#                     mapped['company_name'] = row[1]
-#                     mapped['number_employees'] = row[2]
-#                     mapped['category'] = row[3]
-#                     mapped['city'] = row[4]
-#                     mapped['state'] = row[5]
-#                     mapped['funded_date'] = row[6]
-#                     mapped['raised_amount'] = row[7]
-#                     mapped['raised_currency'] = row[8]
-#                     mapped['round'] = row[9]
-#                     return mapped
-#
-#         if 'state' in options:
-#             for row in csv_data:
-#                 if row[5] == options['state']:
-#                     mapped = {}
-#                     mapped['permalink'] = row[0]
-#                     mapped['company_name'] = row[1]
-#                     mapped['number_employees'] = row[2]
-#                     mapped['category'] = row[3]
-#                     mapped['city'] = row[4]
-#                     mapped['state'] = row[5]
-#                     mapped['funded_date'] = row[6]
-#                     mapped['raised_amount'] = row[7]
-#                     mapped['raised_currency'] = row[8]
-#                     mapped['round'] = row[9]
-#                     return mapped
-#
-#         if 'round' in options:
-#             for row in csv_data:
-#                 if row[9] == options['round']:
-#                     mapped = {}
-#                     mapped['permalink'] = row[0]
-#                     mapped['company_name'] = row[1]
-#                     mapped['number_employees'] = row[2]
-#                     mapped['category'] = row[3]
-#                     mapped['city'] = row[4]
-#                     mapped['state'] = row[5]
-#                     mapped['funded_date'] = row[6]
-#                     mapped['raised_amount'] = row[7]
-#                     mapped['raised_currency'] = row[8]
-#                     mapped['round'] = row[9]
-#                     return mapped
-#
-#         raise RecordNotFound
-#
-#
-# class RecordNotFound(Exception):
-#     pass
+class RecordNotFound(Exception):
+    pass
--- python/test.py
+++ python/test.py
@@ -1,4 +1,4 @@
-from funding_raised import FundingEvent
+from funding_raised import FundingEvent, RecordNotFound
 import unittest
 
 class FundingEventTests(unittest.TestCase):
@@ -8,47 +8,49 @@ class FundingEventTests(unittest.TestCase):
         self.funding_raised = FundingEvent(file_path=self.file_path)
 
     def test_where_returns_events(self):
-        assert len(self.funding_raised.filter_funding_events({'company_name': 'Facebook'})) == 7
+        assert len(self.funding_raised.filter_funding_events({'company_name': 'Facebook'}, False)) == 7
 
     def test_where_returns_correct_keys(self):
         funding_raised = self.funding_raised
-        row = funding_raised.filter_funding_events({'company_name': 'Facebook'})[0]
+        row = funding_raised.filter_funding_events({'company_name': 'Facebook'}, False)[0]
         keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
         values = ['facebook', 'Facebook', 450, 'web', 'Palo Alto', 'CA', '1-Sep-04', 500000, 'USD', 'angel']
         for i in range(0, len(keys)):
             assert row[keys[i]] == values[i]
 
     def test_where_returns_events_by_city(self):
-        assert len(self.funding_raised.filter_funding_events({'city': 'Tempe'})) == 3
+        assert len(self.funding_raised.filter_funding_events({'city': 'Tempe'}, False)) == 3
 
     def test_where_returns_events_by_state(self):
-        assert len(self.funding_raised.filter_funding_events({'state': 'CA'})) == 873
+        assert len(self.funding_raised.filter_funding_events({'state': 'CA'}, False)) == 873
 
     def test_where_returns_events_by_company(self):
-        assert len(self.funding_raised.filter_funding_events({'company_name': 'Facebook', 'round': 'a'})) == 1
+        assert len(self.funding_raised.filter_funding_events({'company_name': 'Facebook', 'round': 'a'}, False)) == 1
 
     def test_where_returns_events_by_type(self):
-        assert len(self.funding_raised.filter_funding_events({'round': 'a'})) == 582
+        assert len(self.funding_raised.filter_funding_events({'round': 'a'}, False)) == 582
 
     def test_where_returns_no_events(self):
-        assert len(self.funding_raised.filter_funding_events({'company_name': 'NotFacebook'})) == 0
+        assert len(self.funding_raised.filter_funding_events({'company_name': 'NotFacebook'}, False)) == 0
+
+    def test_find_by_event_by_company_name(self):
+        row = self.funding_raised.filter_funding_events({'company_name': 'Facebook'}, True)
+        keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
+        values = ['facebook', 'Facebook', 450, 'web', 'Palo Alto', 'CA', '1-Sep-04', 500000, 'USD', 'angel']
+        for i in range(0, len(keys)):
+            assert row[keys[i]] == values[i]
+
+    def test_find_by_event_by_state(self):
+        row = self.funding_raised.filter_funding_events({'state': 'CA'}, True)
+        keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
+        values = ['digg', 'Digg', 60, 'web', 'San Francisco', 'CA', '1-Dec-06', 8500000, 'USD', 'b']
+        for i in range(0, len(keys)):
+            assert row[keys[i]] == values[i]
+
+    def test_raise_record_not_found(self):
+        with self.assertRaises(RecordNotFound) as cm:
+            self.funding_raised.filter_funding_events({'state': 'Space'}, True)
+        self.assertEqual('This funding event does not exist.', str(cm.exception))
 
 if __name__ == '__main__':
     unittest.main()
-
-    # def test_find_by_event_by_company_name():
-    #   row = FundingRaised.find_by({'company_name': 'Facebook'})
-    #   keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
-    #   values = ['facebook', 'Facebook', '450', 'web', 'Palo Alto', 'CA', '1-Sep-04', '500000', 'USD', 'angel']
-    #   for i in range(0, len(keys)):
-    #     assert row[keys[i]] == values[i]
-    #
-    # def test_find_by_event_by_state():
-    #   row = FundingRaised.find_by({'state': 'CA'})
-    #   keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
-    #   values = ['digg', 'Digg', '60', 'web', 'San Francisco', 'CA', '1-Dec-06', '8500000', 'USD', 'b']
-    #   for i in range(0, len(keys)):
-    #     assert row[keys[i]] == values[i]
-
-# test_find_by_event_by_company_name()
-# test_find_by_event_by_state()
