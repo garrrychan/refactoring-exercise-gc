d3e492144a3a76a846a9170b1740ebdd317fba89
Garry <chan6974@gmail.com>
2022-08-10 00:03:07 -0400

    changed: refactored where()

--- python/funding_raised.py
+++ python/funding_raised.py
@@ -1,137 +1,126 @@
-import csv
+from typing import Dict, List
+import pandas as pd
 
-class FundingRaised:
-  @staticmethod
-  def where(options = {}):
-    with open("../startup_funding.csv", "rt") as csvfile:
-      data = csv.reader(csvfile, delimiter=',', quotechar='"')
-      # skip header
-      next(data)
-      csv_data = []
-      for row in data:
-        csv_data.append(row)
 
-    funding = []
-    if 'company_name' in options:
-      result = []
-      for row in csv_data:
-        if row[1] == options['company_name']:
-          result.append(row)
-      csv_data = result
+class FundingEvent:
 
-    if 'city' in options:
-      result = []
-      for row in csv_data:
-        if row[4] == options['city']:
-          result.append(row)
-      csv_data = result
+    def __init__(self, file_path):
+        self.file_path = file_path
 
-    if 'state' in options:
-      result = []
-      for row in csv_data:
-        if row[5] == options['state']:
-          result.append(row)
-      csv_data = result
+    def filter_funding_events(self, fields: Dict) -> List[Dict]:
+        """
+        :param fields: Dict where key represents the csv field column and value represents the str
+        to filter against
+        :return: List of Dict where each Dict represents a funding event row.
+        """
+        df = pd.read_csv(self.file_path)
+        if 'company_name' in fields:
+            df = self.funding_events_by_company(df, fields.get('company_name'))
+        if 'city' in fields:
+            df = self.funding_events_by_city(df, fields.get('city'))
+        if 'state' in fields:
+            df = self.funding_events_by_state(df, fields.get('state'))
+        if 'round' in fields:
+            df = self.funding_events_by_round(df, fields.get('round'))
+        return df.to_dict('records')
 
-    if 'round' in options:
-      result = []
-      for row in csv_data:
-        if row[9] == options['round']:
-          result.append(row)
-      csv_data = result
+    @staticmethod
+    def funding_events_by_company(df: pd.DataFrame, company_name: str) -> pd.DataFrame:
+        df = df[df['company_name'] == company_name]
+        return df
 
-    output = []
-    for row in csv_data:
-      mapped = {}
-      mapped['permalink'] = row[0]
-      mapped['company_name'] = row[1]
-      mapped['number_employees'] = row[2]
-      mapped['category'] = row[3]
-      mapped['city'] = row[4]
-      mapped['state'] = row[5]
-      mapped['funded_date'] = row[6]
-      mapped['raised_amount'] = row[7]
-      mapped['raised_currency'] = row[8]
-      mapped['round'] = row[9]
-      output.append(mapped)
+    @staticmethod
+    def funding_events_by_city(df: pd.DataFrame, city: str) -> pd.DataFrame:
+        df = df[df['city'] == city]
+        return df
 
-    return output
+    @staticmethod
+    def funding_events_by_state(df: pd.DataFrame, state: str) -> pd.DataFrame:
+        df = df[df['state'] == state]
+        return df
 
-  @staticmethod
-  def find_by(options):
-    with open("../startup_funding.csv", "rt") as csvfile:
-      data = csv.reader(csvfile, delimiter=',', quotechar='"')
-      # skip header
-      next(data)
-      csv_data = []
-      for row in data:
-        csv_data.append(row)
+    @staticmethod
+    def funding_events_by_round(df: pd.DataFrame, state: str) -> pd.DataFrame:
+        df = df[df['round'] == state]
+        return df
 
-    if 'company_name' in options:
-      for row in csv_data:
-        if row[1] == options['company_name']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
 
-    if 'city' in options:
-      for row in csv_data:
-        if row[4] == options['city']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    if 'state' in options:
-      for row in csv_data:
-        if row[5] == options['state']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    if 'round' in options:
-      for row in csv_data:
-        if row[9] == options['round']:
-          mapped = {}
-          mapped['permalink'] = row[0]
-          mapped['company_name'] = row[1]
-          mapped['number_employees'] = row[2]
-          mapped['category'] = row[3]
-          mapped['city'] = row[4]
-          mapped['state'] = row[5]
-          mapped['funded_date'] = row[6]
-          mapped['raised_amount'] = row[7]
-          mapped['raised_currency'] = row[8]
-          mapped['round'] = row[9]
-          return mapped
-
-    raise RecordNotFound
-
-class RecordNotFound(Exception):
-  pass
+#     @staticmethod
+#     def find_by(options):
+#         with open("../startup_funding.csv", "rt") as csvfile:
+#             data = csv.reader(csvfile, delimiter=',', quotechar='"')
+#             # skip header
+#             next(data)
+#             csv_data = []
+#             for row in data:
+#                 csv_data.append(row)
+#
+#         if 'company_name' in options:
+#             for row in csv_data:
+#                 if row[1] == options['company_name']:
+#                     mapped = {}
+#                     mapped['permalink'] = row[0]
+#                     mapped['company_name'] = row[1]
+#                     mapped['number_employees'] = row[2]
+#                     mapped['category'] = row[3]
+#                     mapped['city'] = row[4]
+#                     mapped['state'] = row[5]
+#                     mapped['funded_date'] = row[6]
+#                     mapped['raised_amount'] = row[7]
+#                     mapped['raised_currency'] = row[8]
+#                     mapped['round'] = row[9]
+#                     return mapped
+#
+#         if 'city' in options:
+#             for row in csv_data:
+#                 if row[4] == options['city']:
+#                     mapped = {}
+#                     mapped['permalink'] = row[0]
+#                     mapped['company_name'] = row[1]
+#                     mapped['number_employees'] = row[2]
+#                     mapped['category'] = row[3]
+#                     mapped['city'] = row[4]
+#                     mapped['state'] = row[5]
+#                     mapped['funded_date'] = row[6]
+#                     mapped['raised_amount'] = row[7]
+#                     mapped['raised_currency'] = row[8]
+#                     mapped['round'] = row[9]
+#                     return mapped
+#
+#         if 'state' in options:
+#             for row in csv_data:
+#                 if row[5] == options['state']:
+#                     mapped = {}
+#                     mapped['permalink'] = row[0]
+#                     mapped['company_name'] = row[1]
+#                     mapped['number_employees'] = row[2]
+#                     mapped['category'] = row[3]
+#                     mapped['city'] = row[4]
+#                     mapped['state'] = row[5]
+#                     mapped['funded_date'] = row[6]
+#                     mapped['raised_amount'] = row[7]
+#                     mapped['raised_currency'] = row[8]
+#                     mapped['round'] = row[9]
+#                     return mapped
+#
+#         if 'round' in options:
+#             for row in csv_data:
+#                 if row[9] == options['round']:
+#                     mapped = {}
+#                     mapped['permalink'] = row[0]
+#                     mapped['company_name'] = row[1]
+#                     mapped['number_employees'] = row[2]
+#                     mapped['category'] = row[3]
+#                     mapped['city'] = row[4]
+#                     mapped['state'] = row[5]
+#                     mapped['funded_date'] = row[6]
+#                     mapped['raised_amount'] = row[7]
+#                     mapped['raised_currency'] = row[8]
+#                     mapped['round'] = row[9]
+#                     return mapped
+#
+#         raise RecordNotFound
+#
+#
+# class RecordNotFound(Exception):
+#     pass
--- python/test.py
+++ python/test.py
@@ -1,50 +1,54 @@
-from funding_raised import FundingRaised
+from funding_raised import FundingEvent
+import unittest
 
-def test_where_returns_events():
-  assert len(FundingRaised.where({'company_name': 'Facebook'})) == 7
+class FundingEventTests(unittest.TestCase):
 
-def test_where_returns_correct_keys():
-  row = FundingRaised.where({'company_name': 'Facebook'})[0]
-  keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
-  values = ['facebook', 'Facebook', '450', 'web', 'Palo Alto', 'CA', '1-Sep-04', '500000', 'USD', 'angel']
-  for i in range(0, len(keys)):
-    assert row[keys[i]] == values[i]
+    def setUp(self):
+        self.file_path = '../startup_funding.csv'
+        self.funding_raised = FundingEvent(file_path=self.file_path)
 
-def test_where_returns_events_by_city():
-  assert len(FundingRaised.where({'city': 'Tempe'})) == 3
+    def test_where_returns_events(self):
+        assert len(self.funding_raised.filter_funding_events({'company_name': 'Facebook'})) == 7
 
-def test_where_returns_events_by_state():
-  assert len(FundingRaised.where({'state': 'CA'})) == 873
+    def test_where_returns_correct_keys(self):
+        funding_raised = self.funding_raised
+        row = funding_raised.filter_funding_events({'company_name': 'Facebook'})[0]
+        keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
+        values = ['facebook', 'Facebook', 450, 'web', 'Palo Alto', 'CA', '1-Sep-04', 500000, 'USD', 'angel']
+        for i in range(0, len(keys)):
+            assert row[keys[i]] == values[i]
 
-def test_where_returns_events_by_company():
-  assert len(FundingRaised.where({'company_name': 'Facebook', 'round': 'a'})) == 1
+    def test_where_returns_events_by_city(self):
+        assert len(self.funding_raised.filter_funding_events({'city': 'Tempe'})) == 3
 
-def test_where_returns_events_by_type():
-  assert len(FundingRaised.where({'round': 'a'})) == 582
+    def test_where_returns_events_by_state(self):
+        assert len(self.funding_raised.filter_funding_events({'state': 'CA'})) == 873
 
-def test_where_returns_no_events():
-  assert len(FundingRaised.where({'company_name': 'NotFacebook'})) == 0
+    def test_where_returns_events_by_company(self):
+        assert len(self.funding_raised.filter_funding_events({'company_name': 'Facebook', 'round': 'a'})) == 1
 
-def test_find_by_event_by_company_name():
-  row = FundingRaised.find_by({'company_name': 'Facebook'})
-  keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
-  values = ['facebook', 'Facebook', '450', 'web', 'Palo Alto', 'CA', '1-Sep-04', '500000', 'USD', 'angel']
-  for i in range(0, len(keys)):
-    assert row[keys[i]] == values[i]
+    def test_where_returns_events_by_type(self):
+        assert len(self.funding_raised.filter_funding_events({'round': 'a'})) == 582
 
-def test_find_by_event_by_state():
-  row = FundingRaised.find_by({'state': 'CA'})
-  keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
-  values = ['digg', 'Digg', '60', 'web', 'San Francisco', 'CA', '1-Dec-06', '8500000', 'USD', 'b']
-  for i in range(0, len(keys)):
-    assert row[keys[i]] == values[i]
+    def test_where_returns_no_events(self):
+        assert len(self.funding_raised.filter_funding_events({'company_name': 'NotFacebook'})) == 0
 
-test_where_returns_events()
-test_where_returns_correct_keys()
-test_where_returns_events_by_city()
-test_where_returns_events_by_state()
-test_where_returns_events_by_company()
-test_where_returns_events_by_type()
-test_where_returns_no_events()
-test_find_by_event_by_company_name()
-test_find_by_event_by_state()
+if __name__ == '__main__':
+    unittest.main()
+
+    # def test_find_by_event_by_company_name():
+    #   row = FundingRaised.find_by({'company_name': 'Facebook'})
+    #   keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
+    #   values = ['facebook', 'Facebook', '450', 'web', 'Palo Alto', 'CA', '1-Sep-04', '500000', 'USD', 'angel']
+    #   for i in range(0, len(keys)):
+    #     assert row[keys[i]] == values[i]
+    #
+    # def test_find_by_event_by_state():
+    #   row = FundingRaised.find_by({'state': 'CA'})
+    #   keys = ['permalink', 'company_name', 'number_employees', 'category', 'city', 'state', 'funded_date', 'raised_amount', 'raised_currency', 'round']
+    #   values = ['digg', 'Digg', '60', 'web', 'San Francisco', 'CA', '1-Dec-06', '8500000', 'USD', 'b']
+    #   for i in range(0, len(keys)):
+    #     assert row[keys[i]] == values[i]
+
+# test_find_by_event_by_company_name()
+# test_find_by_event_by_state()
